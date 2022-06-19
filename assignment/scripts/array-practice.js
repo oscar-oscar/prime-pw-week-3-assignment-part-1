72af0c1f7116dfd4ed60bd3fc13e546679fa493e
Oscar Carbajal <oscar.carbajal.work@gmail.com>
2022-06-19 14:50:20 -0500

    sepearted array(1a) & console(1b) to lines 12 annd 18

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -10,11 +10,12 @@ console.log('Animals are: ', animalArray);
 // 1.a. TODO: Make an array with some favorite foods
 
 let favFoods = ['tacos', 'burgers', 'pho', 'sandwiches'];
-console.log('These are some of my favorite foods.', favFoods);
+
 
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
+console.log('These are some of my favorite foods.', favFoods);
 
 
 
